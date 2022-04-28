66220ac2a33dc693bccff206857f328f8c7bb5bd
Vener Baimuratov <Ve.bai@mail.ru>
2022-04-28 22:28:14 +0300

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,17 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+let sum = 0;
+
 function sumSalary(salaries) {
-  // ваш код...
+	for (let key in salaries) {
+    if( typeof salaries[key] === 'number') {
+      sum += salaries[key];
+    } 
+  }  return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+  a: 10,
+  b: 5,
+	read (){
+    this.a = +prompt('Введите значение a:', '');
+    this.b = +prompt('Введите значение b:', '');
+    
+  },
+  sum (){
+    return (this.a + this.b);
+  },
+  mul () {
+    return (this.a * this.b);
+  },
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
