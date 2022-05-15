24e389ebe972246bdf12220253e14505c208a133
Vener Baimuratov <Ve.bai@mail.ru>
2022-05-15 14:19:22 +0300

    module -4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('UL');
+  for (human of friends) {
+  let li = document.createElement('LI')
+  ul.append(li)
+  li.textContent = `${human.firstName} ${human.lastName}`;
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    
+    if (table.rows[i].cells[2].innerHTML === 'm') {
+      table.rows[i].classList.add('male');
+    }
+    if (table.rows[i].cells[2].innerHTML === 'f') {
+      table.rows[i].classList.add('female');
+    }
+  } 
+  for (let i = 1; i < table.rows.length; i++) {  
+    if (table.rows[i].cells[1].innerHTML < 18) {
+      table.rows[i].style = "text-decoration: line-through"; 
+    }
+  }  
+
+  for (let i = 1; i < table.rows.length; i++) {
+
+    if (table.rows[i].cells[3].dataset.available === 'true') {
+      table.rows[i].classList.toggle('available'); 
+    }
+    if (table.rows[i].cells[3].dataset.available === 'false') {
+      table.rows[i].classList.toggle('unavailable'); 
+    } 
+    if (table.rows[i].cells[3].hasAttribute('data-available') == false) {
+      table.rows[i].hidden = true;
+    } else table.rows[i].hidden = null;
+
+}
 }
