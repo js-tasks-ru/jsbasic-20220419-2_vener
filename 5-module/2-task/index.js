47dd3ca356636693b1e9ad4f72fd80323b7ffca2
Vener Baimuratov <Ve.bai@mail.ru>
2022-05-22 16:46:35 +0300

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
-}
+  let button = document.querySelector('.toggle-text-button')
+  let text =document.getElementById('text')
+  
+  button.addEventListener('click', function(){
+      if(!text.hidden) text.hidden = true
+      else{
+        text.hidden = false
+      }
+  })
+  }
